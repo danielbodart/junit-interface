a30f3eca2e09cc2c6f9cf49de85bc888107e8721
Bryan J Swift <bryan.j.swift@gmail.com>
2010-04-17 15:44:53 -0400

    Change the version of sbt-plugins
    Makes it possible to publish again.

--- project/plugins/Plugins.scala
+++ project/plugins/Plugins.scala
@@ -4,5 +4,5 @@ class Plugins(info:ProjectInfo) extends PluginDefinition(info) {
 	// Repository location for sbt-plugins
 	val bryanjswift = "Bryan J Swift" at "http://repos.bryanjswift.com/maven2/"
 	// sbt-plugins
-	val plugins = "bryanjswift" % "sbt-plugins" % "0.4.1"
+	val plugins = "bryanjswift" % "sbt-plugins" % "0.4.3"
 }
